e2782f3c30e4379ebf978ad006b7f5216bf0b32e
acuestar <47720434+acuestar@users.noreply.github.com>
2019-03-19 14:04:27 +0100

    Update MainController.java

--- src/main/java/com/api/rest/controller/MainController.java
+++ src/main/java/com/api/rest/controller/MainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ResponseEntity rootResponse() {
-        String welcomeMessage = "Welcome to the Aratech API";
+        String welcomeMessage = "Welcome to the Rest API";
         return new ResponseEntity<String>(welcomeMessage, HttpStatus.OK);
     }
 
